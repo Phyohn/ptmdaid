91c8482cfad27a87cb9bcc3c1274f86425d903c3
MTKSH <friktion0001@gmail.com>
2022-02-08 13:01:43 +0900

    rebuild

--- sep.py
+++ sep.py
@@ -1,46 +1,27 @@
 # sep.py
 #python -i
 #test
-
-import cv2
 import base64
-import numpy as np
-from PIL import Image
-import io
-import os
-import pathlib
-import datetime
-import time
-import platform
 import codecs
+import cv2
+import datetime
+import io
+import numpy as np
+import os
 import pandas as pd
+import pathlib
+from PIL import Image
+import platform
+import re
+import time
 
-#data = pd.read_csv('./Raw/daidata.csv',names=('Rotation','BB','RB','co1','co2','han1','han2','difference','han3','max','dai','machine','holl','date','kari1','kari2','url','text'), encoding = "utf-16")
+def f_int(x):
+	return str(x).replace(',','')
+#def (ezkana_comp.drop_duplicates(subset=['dai'])).sort_values('dai')
 
-rawdata = pd.read_csv('./Raw/daidata.csv', encoding = 'utf-16')
+rawdata = pd.read_csv('./Raw/daidata.csv', encoding = 'utf-16' ,dtype=str)
 #all chenge columnsname
 rawdata = rawdata.set_axis(['Rotation','BB','RB','co1','co2','han1','han2','difference','han3','max','dai','machine','holl','date','kari1','kari2','url','text'], axis='columns')
-
-#drop_duplicates(['dai','holl']) bool
-rawdata = rawdata.drop_duplicates(['dai','holl'])
-
-#Preprocessing
-#Remove the comma (Because NaN may be created after removing it)
-##important ! 1,234 are treated as strings and replaced by the replace(',','')
-#When replacing for one value, it will be removed by A.replace (',',''), but for df, series,use .str.replace.(',','')
-#You can change to int type only by removing.
-
-rawdata['Rotation'] = rawdata['Rotation'].str.replace(',','')
-rawdata['max'] = rawdata['max'].str.replace(',','')
-
-#Fill in missing values ​​with 0
-rawdata = rawdata.fillna(0)
-
-#Judgment whether there is NaN for each column True is returned for columns with missing values
-print(rawdata.isnull().any())
-
-#Type conversion
-rawdata = rawdata.astype({'Rotation':'int64','BB':'int64','RB':'int64','co1':'int64','co2':'int64','han2':'int64','max':'int64','machine':'str','kari1':'int64','kari2':'int64'})
 
 #y/n date today?
 #y/Ndef
@@ -65,136 +46,90 @@ print(intdt)
 #'date'.values replace intdt all
 rawdata['date'] = intdt
 
-#Type confirmation
-rawdata.dtypes
+#replace('-','0')
+rawdata = rawdata.replace('-','0')
 
-#Common up to here
+#each holl dai num check
+zero_rawdata = rawdata[rawdata['holl'] == 'ゼロタイガー']
+zero_rawdata.insert(0,'dai_num_check',zero_rawdata.loc[:,'dai'])
+dai_num_check_df = pd.read_csv('./tmp/zerodailist.csv',names=('dai_num_check','empty'),dtype=str)
+zero_outer_merged_df = pd.merge(zero_rawdata, dai_num_check_df, how='outer')
+zero_inner_merged_df = pd.merge(zero_outer_merged_df, dai_num_check_df, how='inner')
+zero_inner_merged_df['holl'] = zero_inner_merged_df['holl'].fillna(method='ffill')
 
-#if Sentence "hollname" in df.values
-if 'ゼロタイガー' in rawdata.values:
-	zerodf = rawdata.reindex(columns=['dai','Rotation','BB','RB','difference','max','machine','date','holl'])
-	zerodf = zerodf[zerodf['holl'] == 'ゼロタイガー']
-	zerodf = zerodf.iloc[:,:8]
-	#'difference'chenge .astype to int64 
-	zerodf['difference'] = zerodf['difference'].str.replace(',','')
-	zerodf = zerodf.astype({'difference':'int64'})
-	#auto seriesmachine bank
-	#pd.Series.unique()
-	zeroser = zerodf.loc[:,'machine'].unique()
-	#series to df
-	zeroserdf = pd.DataFrame(zeroser)
-	zeroserdf.insert(0,'namebank', zeroser)
-	dainame = pd.read_csv('./tmp/namebank.csv',names=('namebank','neoname'))
-	#drop_duplicates(subset=['namebank']
-	dainame = dainame.drop_duplicates(subset=['namebank'])
-	newdailist = pd.merge(zeroserdf, dainame, how='outer')
-	newdailist = newdailist.reindex(columns=['namebank','neoname'])
-	newdailist.to_csv('./tmp/namebank.csv', header=False, index=False)
-	#name.txt to String conversion
-	dainame = pd.read_csv('./tmp/namebank.csv', header=None)
-	#tolist
-	machinename = (dainame.iloc[:,0]).values.tolist()
-	newname = (dainame.iloc[:,1]).values.tolist()
-	#replace
-	zerocomp = zerodf.replace(machinename,newname)
-	zerocomp.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/zero.csv', header=False, index=False)
-	print ('zero')
-else:
-	print ('no zero')
+ezkana_rawdata = rawdata[rawdata['holl'] == 'eZone金沢']
+ezkana_rawdata.insert(0,'dai_num_check',ezkana_rawdata.loc[:,'dai'])
+dai_num_check_df = pd.read_csv('./tmp/ezkanadailist.csv',names=('dai_num_check','empty'),dtype=str)
+ezkana_outer_merged_df = pd.merge(ezkana_rawdata, dai_num_check_df, how='outer')
+ezkana_inner_merged_df = pd.merge(ezkana_outer_merged_df, dai_num_check_df, how='inner')
+ezkana_inner_merged_df['holl'] = ezkana_inner_merged_df['holl'].fillna(method='ffill')
 
-#case ezkana or moro to Calculation
-if 'eZone金沢' in rawdata.values or 'オークラ諸江' in rawdata.values:
-	Recal = rawdata.reindex(columns=['han2','kari1','kari2','dai','Rotation','BB','RB','difference','max','machine','date','holl'])
-	#diff(('kari1'/1000)*6)*('kari2'-'han2')
-	#Calculation
-	Recal['difference'] = ((Recal['kari1'] / 1000) * 6)  * (Recal['kari2'] - Recal['han2'])
-	Recaled = Recal.reindex(columns=['dai','Rotation','BB','RB','difference','max','machine','date','holl',])
-	#['difference'] to int64
-	Recaled = Recaled.astype({'difference':'int64'})
-else:
-	print ('fin')
 
-#case ezkana
-if 'eZone金沢' in Recaled.values:
-	#bloom to df
-	#Recaled['holl'] == 'eZone金沢'
-	ezkanadf = Recaled[Recaled['holl'] == 'eZone金沢']
-	ezkanadf = ezkanadf.iloc[:,:8]
-	#dailist
-	posdaiez = ezkanadf.loc[:,'dai']
-	ezkanadf.insert(0,'posdai',posdaiez)
-	dailist = pd.read_csv('./tmp/ezkanadailist.csv',names=('posdai','kuu'))
-	ezkanadf = pd.merge(ezkanadf, dailist, how='outer')
-	ezkanadf = ezkanadf.reindex(columns=['posdai','Rotation','BB','RB','difference','max','machine','date'])
-	#fillna(method='ffill') to 'date'
-	ezkanadf['date'] = ezkanadf['date'].fillna(method='ffill')
-	ezkanadf= ezkanadf.fillna(0)
-	ezkanadf = ezkanadf.astype({'posdai':'int64','Rotation':'int64','BB':'int64','RB':'int64','difference':'int64','max':'int64','machine':'str','date':'int64'})
-	ezkanadf = ezkanadf.sort_values('posdai')
-	#auto seriesmachine bank
-	#pd.Series.unique()
-	ezkanaser = ezkanadf.loc[:,'machine'].unique()
-	#series to df
-	ezkanaserdf = pd.DataFrame(ezkanaser)
-	ezkanaserdf.insert(0,'namebank', ezkanaser)
-	dainame = pd.read_csv('./tmp/namebank.csv',names=('namebank','neoname'))
-	#drop_duplicates(subset=['namebank']
-	dainame = dainame.drop_duplicates(subset=['namebank'])
-	newdailist = pd.merge(ezkanaserdf, dainame, how='outer')
-	newdailist = newdailist.reindex(columns=['namebank','neoname'])
-	newdailist.to_csv('./tmp/namebank.csv', header=False, index=False)
-	#name.txt to String conversion
-	dainame = pd.read_csv('./tmp/namebank.csv', header=None)
-	#tolist
-	machinename = (dainame.iloc[:,0]).values.tolist()
-	newname = (dainame.iloc[:,1]).values.tolist()
-	#replace
-	ezkanacomp = ezkanadf.replace(machinename,newname)
-	ezkanacomp.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/ezkana.csv', header=False, index=False)
-	print('ezkana')
-else:
-	print('no ezkana')
+moroe_rawdata = rawdata[rawdata['holl'] == 'オークラ諸江']
+moroe_rawdata.insert(0,'dai_num_check',moroe_rawdata.loc[:,'dai'])
+dai_num_check_df = pd.read_csv('./tmp/moroedailist.csv',names=('dai_num_check','empty'),dtype=str)
+moroe_outer_merged_df = pd.merge(moroe_rawdata, dai_num_check_df, how='outer')
+moroe_inner_merged_df = pd.merge(moroe_outer_merged_df, dai_num_check_df, how='inner')
+moroe_inner_merged_df['holl'] = moroe_inner_merged_df['holl'].fillna(method='ffill')
 
-#case moro
-if 'オークラ諸江' in Recaled.values:
-	#bloom to df
-	#Recaled['holl'] == 'オークラ諸江'
-	morodf = Recaled[Recaled['holl'] == 'オークラ諸江']
-	morodf = morodf.iloc[:,:8]
-	#dailist
-	posdai = morodf.loc[:,'dai']
-	morodf.insert(0,'posdai',posdai)
-	dailist = pd.read_csv('./tmp/moroedailist.csv',names=('posdai','kuu'))
-	morodf = pd.merge(morodf, dailist, how='outer')
-	morodf = morodf.reindex(columns=['posdai','Rotation','BB','RB','difference','max','machine','date'])
-	#fillna(method='ffill') to 'date'
-	morodf['date'] = morodf['date'].fillna(method='ffill')
-	morodf = morodf.fillna(0)
-	morodf = morodf.astype({'posdai':'int64','Rotation':'int64','BB':'int64','RB':'int64','difference':'int64','max':'int64','machine':'str','date':'int64'})
-	morodf = morodf.sort_values('posdai')
-	#auto seriesmachine bank
-	#pd.Series.unique()
-	moroser = morodf.loc[:,'machine'].unique()
-	#series to df
-	moroserdf = pd.DataFrame(moroser)
-	moroserdf.insert(0,'namebank', moroser)
-	dainame = pd.read_csv('./tmp/namebank.csv',names=('namebank','neoname'))
-	#drop_duplicates(subset=['namebank']
-	dainame = dainame.drop_duplicates(subset=['namebank'])
-	newdailist = pd.merge(moroserdf, dainame, how='outer')
-	newdailist = newdailist.reindex(columns=['namebank','neoname'])
-	newdailist.to_csv('./tmp/namebank.csv', header=False, index=False)
-	#name.txt to String conversion
-	dainame = pd.read_csv('./tmp/namebank.csv', header=None)
-	#tolist
-	machinename = (dainame.iloc[:,0]).values.tolist()
-	newname = (dainame.iloc[:,1]).values.tolist()
-	#replace
-	morocomp = morodf.replace(machinename,newname)
-	morocomp.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/moroe.csv', header=False, index=False)
-	print('moro')
-else:
-	print('no moro')
+#concat
+concated_rawdata = pd.concat([zero_inner_merged_df, ezkana_inner_merged_df, moroe_inner_merged_df])
+concated_rawdata.dtypes
 
-print('fin')
+#auto seriesmachine bank
+#pd.Series.drop_duplicates()
+machine_name_df = pd.DataFrame(concated_rawdata['machine'].drop_duplicates())
+machine_name_df['hoge'] = '0'
+rename_list_df = pd.read_csv('./tmp/namebank.csv',names=('machine','renamed_machine_name'))
+merged_machine_name_df = pd.merge(machine_name_df, rename_list_df , how='outer')
+new_machine_list_df = merged_machine_name_df .reindex(columns=['machine','renamed_machine_name'])
+new_machine_list = new_machine_list_df.sort_values('renamed_machine_name', na_position='first')
+new_machine_list.to_csv('./tmp/namebank.csv', header=False, index=False)
+
+
+
+#trim
+trim_rawdata = (concated_rawdata.applymap(f_int)).fillna(0).replace('nan','0')
+#trim_rawdata.isnull().any()
+
+#Calculation
+rawdata_to_int_df = trim_rawdata.reindex(columns=['han2','kari1','kari2','dai_num_check','Rotation','BB','RB','max','date','difference','machine','holl'])
+cal = rawdata_to_int_df
+cal_int64 = cal.astype({'han2':'int64','kari1':'int64','kari2':'int64'})
+cal_int64['cal_difference'] = ((cal_int64['kari1'] / 1000) * 6) * (cal_int64['kari2'] - cal_int64['han2'])
+calculated_df = cal_int64.astype({'cal_difference':'int64'})
+
+#rename
+machine = (new_machine_list.iloc[:,0]).values.tolist()
+renamed_machine_name = (new_machine_list.iloc[:,1]).values.tolist()
+calculated_df['renamed'] = calculated_df['machine'].replace(machine,renamed_machine_name)
+
+comp_df = calculated_df.reindex(columns=['dai_num_check','Rotation','BB','RB','difference','cal_difference','max','renamed','date','holl'])
+comp_df['date'] = intdt
+
+#zero
+zero_df = comp_df[comp_df['holl'] == 'ゼロタイガー']
+zero_comp = zero_df.reindex(columns=['dai_num_check','Rotation','BB','RB','difference','max','renamed','date'])
+int_zero_comp = zero_comp.astype({'dai_num_check':'int64','Rotation':'int64','BB':'int64','RB':'int64','difference':'int64','max':'int64','renamed':'str','date':'int64'})
+zero_csv = (int_zero_comp.drop_duplicates(subset=['dai_num_check'])).sort_values('dai_num_check')
+zero_csv.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/zero.csv', header=False, index=False)
+
+
+#ezkana
+ezkana_df = comp_df[comp_df['holl'] == 'eZone金沢']
+ezkana_comp = ezkana_df.reindex(columns=['dai_num_check','Rotation','BB','RB','cal_difference','max','renamed','date'])
+int_ezkana_csv = ezkana_comp.astype({'dai_num_check':'int64','Rotation':'int64','BB':'int64','RB':'int64','cal_difference':'int64','max':'int64','renamed':'str','date':'int64'})
+ezkana_csv = (int_ezkana_csv.drop_duplicates(subset=['dai_num_check'])).sort_values('dai_num_check')
+ezkana_csv.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/ezkana.csv', header=False, index=False)
+
+#moroe
+moroe_df = comp_df[comp_df['holl'] == 'オークラ諸江']
+moroe_comp = moroe_df.reindex(columns=['dai_num_check','Rotation','BB','RB','cal_difference','max','renamed','date'])
+int_moroe_comp = moroe_comp.astype({'dai_num_check':'int64','Rotation':'int64','BB':'int64','RB':'int64','cal_difference':'int64','max':'int64','renamed':'str','date':'int64'})
+moroe_csv = (int_moroe_comp.drop_duplicates(subset=['dai_num_check'])).sort_values('dai_num_check')
+moroe_csv.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/moroe.csv', header=False, index=False)
+
+
+#moroe_df.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/moroe.csv', header=False, index=False)
+
 quit()
